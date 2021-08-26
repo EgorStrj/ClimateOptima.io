55fc0217da923b508414ed4aa8e51147d469dd0a
Egor Astrejko <egor.strj@egmail.com>
2021-08-26 18:43:44 +0300

    1.4 New

---
+++ js/catalog.js
@@ -0,0 +1,14 @@
+'use strict'
+let links = document.querySelectorAll('.categories__list-item-link');
+console.log(links);
+for (let link of links) {
+    link.addEventListener("click", function (e) {
+        if (link === e.target)
+        {
+            for (let link of links) {
+                link.classList.remove("active")
+            }
+            link.classList.add("active")
+        }
+    })
+}
--- js/main.js
+++ js/main.js
@@ -34,19 +34,7 @@ window.onload
         })
     }
 
-    let links = document.querySelectorAll('.categories__list-item-link');
-    console.log(links);
-    for (let link of links) {
-        link.addEventListener("click", function (e) {
-            if (link === e.target)
-            {
-                for (let link of links) {
-                    link.classList.remove("active")
-                }
-                link.classList.add("active")
-            }
-        })
-    }
+
 
 
 }
