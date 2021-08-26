6feeadf513bde300d785e1c1b36e20f098bcb43e
Egor Astrejko <egor.strj@egmail.com>
2021-08-27 00:58:22 +0300

    1.5

--- js/catalog.js
+++ js/catalog.js
@@ -1,14 +1,16 @@
-'use strict'
-let links = document.querySelectorAll('.categories__list-item-link');
-console.log(links);
-for (let link of links) {
-    link.addEventListener("click", function (e) {
-        if (link === e.target)
-        {
-            for (let link of links) {
-                link.classList.remove("active")
+window.onload
+{
+    let links = document.querySelectorAll('.categories__list-item-link');
+    console.log(links);
+    for (let link of links) {
+        link.addEventListener("click", function (e) {
+            if (link === e.target)
+            {
+                for (let link of links) {
+                    link.classList.remove("active")
+                }
+                link.classList.add("active")
             }
-            link.classList.add("active")
-        }
-    })
+        })
+    }
 }
--- js/main.js
+++ js/main.js
@@ -11,8 +11,19 @@ window.onload
     })
 
 
-    const anchors = document.querySelectorAll('a.menu__item[href*="#"]');
 
+
+    let menuBtn = document.getElementsByClassName('menu__open').item(0);
+    menuBtn.addEventListener('click', function () {
+        menu.classList.add('active');
+    })
+
+
+
+
+
+    const anchors = document.querySelectorAll('a.menu__item[href*="#"]');
+    let menu = document.getElementsByClassName('menu').item(0);
     for (let anchor of anchors) {
         anchor.addEventListener("click", function (e) {
             if (e.target !== anchors.item(1)) {
@@ -22,19 +33,10 @@ window.onload
                     behavior: "smooth",
                     block: "center"
                 })
+                menu.classList.remove('active')
             }
-            // if (e.target === anchors.item(2)) {
-            //     e.preventDefault();
-            //     const blockId = anchor.getAttribute('href')
-            //     document.querySelector('' + blockId).scrollIntoView({
-            //         behavior: "smooth",
-            //         block: "center"
-            //     })
-            // }
         })
     }
-
-
 
 
 }
